801f5ebda548b1019647f1e6fb6ce0a28a6e6dd3
mariacastro11 <maria1.castro.stj@gmail.com>
2025-03-13 23:10:11 -0500

    crear componente BlogPage extendiendo HTMLElement (BlogPage.js)

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -2,5 +2,16 @@ import "../Components/BlogHeader.js";
 import "../Components/BlogMain.js";
 import "../Components/BlogFooter.js";
 
+class BlogPage extends HTMLElement{
+    constructor() {
+        super();
+    }
+
+    Connectcallback (){
+        this.render();
+    }
+    
+    
+}
 
 
