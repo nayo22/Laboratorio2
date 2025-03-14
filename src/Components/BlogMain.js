d1d545f602a1da65d79140ab82c9747e0c29c7be
nayo <skynet8252@gmail.com>
2025-03-13 21:24:49 -0500

    feat: crear componente blog-main con layout básico de posts(BlogHeader.js)

---
+++ src/Components/BlogMain.js
@@ -0,0 +1,23 @@
+class BlogMain extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = `
+     <main class="main">
+     <article class="post">
+      <h2>Post</h2>
+      <p>Post content goes here...</p>
+      </article>
+      </main>
+    `;
+  }
+
+}
+
+customElements.define('blog-header', BlogHeader);
